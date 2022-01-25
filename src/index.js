405f48697dc99b009868e624199b9251261cc4a8
JKing <lazy2000person@gmail.com>
2022-01-25 17:04:43 +0800

    处理函数

--- src/index.js
+++ src/index.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function sayHello() {
+  return 'Hello'
+}
+
+const list_obj = [
+  {
+    title: 'route',
+    url: 'https://react.js',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
+
+const List = () => {
+  return list_obj.map(item => (<div key={item.objectID}>
+    <li>{item.title}</li>
+    <li>{item.url}</li>
+    <li>{item.author}</li>
+  </div>)
+  )
+}
+
+class App extends Component {
+  render() {
+    const title = 'React'
+    const handleChange = event => {
+      console.log(event.target.value)
+    }
+    return (<div>
+      {sayHello()}
+      <br />
+      {title}
+      <br />
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <hr />
+      <List />
+    </div>)
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
