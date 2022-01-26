3f00bc1bd32d04e8fbab072df6466b606d140be2
JKing <lazy2000person@gmail.com>
2022-01-26 15:12:48 +0800

    分离Search

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
@@ -7,27 +7,9 @@ function sayHello() {
   return 'Hello'
 }
 
-const list_obj = [
-  {
-    title: 'route',
-    url: 'https://react.js',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-]
 
-const List = () => {
-  return list_obj.map(item => (<div key={item.objectID}>
+const List = (props) => {
+  return props.list.map(item => (<div key={item.objectID}>
     <li>{item.title}</li>
     <li>{item.url}</li>
     <li>{item.author}</li>
@@ -35,23 +17,56 @@ const List = () => {
   )
 }
 
-class App extends Component {
-  render() {
-    const title = 'React'
-    const handleChange = event => {
-      console.log(event.target.value)
-    }
-    return (<div>
-      {sayHello()}
-      <br />
-      {title}
-      <br />
+const Search = () => {
+  const handleChange = event => {
+    setSearchTerm(event.target.value)
+    console.log(event.target.value)
+  }
+  const [searchTerm, setSearchTerm] = React.useState('Hello React~~');
+  return (
+    <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
-      <hr />
-      <List />
-    </div>)
-  }
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const stories = [
+    {
+      title: 'route',
+      url: 'https://react.js',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ]
+  const title = 'React';
+
+  return (<div>
+    {sayHello()}
+    <br />
+    {title}
+    <br />
+
+    <Search />
+    <hr />
+    <List list={stories} />
+  </div>)
+
 }
 
 ReactDOM.render(
